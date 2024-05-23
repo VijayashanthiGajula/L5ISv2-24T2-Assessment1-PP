632f9ec73f373c8b041c973cad2fe939cf8a1f4b
vijaya shanthi gajulagit config --global user.email <vijayashanthi.gajula@gmail.com>
2024-05-24 09:52:48 +1200

    Add project files.

---
+++ CourseEnrollment/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace CourseEnrollment
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNameTxt = new System.Windows.Forms.TextBox();
+            this.FNLbl = new System.Windows.Forms.Label();
+            this.DOBlbl = new System.Windows.Forms.Label();
+            this.PHlbl = new System.Windows.Forms.Label();
+            this.PhoneTxt = new System.Windows.Forms.TextBox();
+            this.LNlbl = new System.Windows.Forms.Label();
+            this.LastNameTxt = new System.Windows.Forms.TextBox();
+            this.FinalOutputListBox = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.PersonalGroup = new System.Windows.Forms.GroupBox();
+            this.EmailTxt = new System.Windows.Forms.TextBox();
+            this.Emaillbl = new System.Windows.Forms.Label();
+            this.DOB_Dtp = new System.Windows.Forms.DateTimePicker();
+            this.genderComboBox = new System.Windows.Forms.ComboBox();
+            this.GenderLbl = new System.Windows.Forms.Label();
+            this.CourseGroup = new System.Windows.Forms.GroupBox();
+            this.courseComboBox = new System.Windows.Forms.ComboBox();
+            this.ListedInfoGroup = new System.Windows.Forms.GroupBox();
+            this.PersonalGroup.SuspendLayout();
+            this.CourseGroup.SuspendLayout();
+            this.ListedInfoGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FirstNameTxt
+            // 
+            this.FirstNameTxt.Location = new System.Drawing.Point(116, 30);
+            this.FirstNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FirstNameTxt.Name = "FirstNameTxt";
+            this.FirstNameTxt.Size = new System.Drawing.Size(163, 20);
+            this.FirstNameTxt.TabIndex = 0;
+            // 
+            // FNLbl
+            // 
+            this.FNLbl.AutoSize = true;
+            this.FNLbl.Location = new System.Drawing.Point(41, 35);
+            this.FNLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.FNLbl.Name = "FNLbl";
+            this.FNLbl.Size = new System.Drawing.Size(57, 13);
+            this.FNLbl.TabIndex = 1;
+            this.FNLbl.Text = "First Name";
+            // 
+            // DOBlbl
+            // 
+            this.DOBlbl.AutoSize = true;
+            this.DOBlbl.Location = new System.Drawing.Point(37, 102);
+            this.DOBlbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DOBlbl.Name = "DOBlbl";
+            this.DOBlbl.Size = new System.Drawing.Size(66, 13);
+            this.DOBlbl.TabIndex = 3;
+            this.DOBlbl.Text = "Date of Birth";
+            // 
+            // PHlbl
+            // 
+            this.PHlbl.AutoSize = true;
+            this.PHlbl.Location = new System.Drawing.Point(23, 167);
+            this.PHlbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.PHlbl.Name = "PHlbl";
+            this.PHlbl.Size = new System.Drawing.Size(78, 13);
+            this.PHlbl.TabIndex = 7;
+            this.PHlbl.Text = "Phone Number";
+            // 
+            // PhoneTxt
+            // 
+            this.PhoneTxt.Location = new System.Drawing.Point(116, 166);
+            this.PhoneTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PhoneTxt.Name = "PhoneTxt";
+            this.PhoneTxt.Size = new System.Drawing.Size(162, 20);
+            this.PhoneTxt.TabIndex = 6;
+            // 
+            // LNlbl
+            // 
+            this.LNlbl.AutoSize = true;
+            this.LNlbl.Location = new System.Drawing.Point(41, 68);
+            this.LNlbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LNlbl.Name = "LNlbl";
+            this.LNlbl.Size = new System.Drawing.Size(58, 13);
+            this.LNlbl.TabIndex = 13;
+            this.LNlbl.Text = "Last Name";
+            // 
+            // LastNameTxt
+            // 
+            this.LastNameTxt.Location = new System.Drawing.Point(117, 66);
+            this.LastNameTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LastNameTxt.Name = "LastNameTxt";
+            this.LastNameTxt.Size = new System.Drawing.Size(162, 20);
+            this.LastNameTxt.TabIndex = 1;
+            // 
+            // FinalOutputListBox
+            // 
+            this.FinalOutputListBox.FormattingEnabled = true;
+            this.FinalOutputListBox.Location = new System.Drawing.Point(13, 26);
+            this.FinalOutputListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FinalOutputListBox.Name = "FinalOutputListBox";
+            this.FinalOutputListBox.Size = new System.Drawing.Size(252, 342);
+            this.FinalOutputListBox.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(228, 21);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 26);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Enrollment Form";
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.Location = new System.Drawing.Point(107, 432);
+            this.SubmitBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(160, 33);
+            this.SubmitBtn.TabIndex = 10;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            // 
+            // PersonalGroup
+            // 
+            this.PersonalGroup.Controls.Add(this.EmailTxt);
+            this.PersonalGroup.Controls.Add(this.Emaillbl);
+            this.PersonalGroup.Controls.Add(this.DOB_Dtp);
+            this.PersonalGroup.Controls.Add(this.genderComboBox);
+            this.PersonalGroup.Controls.Add(this.LNlbl);
+            this.PersonalGroup.Controls.Add(this.FirstNameTxt);
+            this.PersonalGroup.Controls.Add(this.FNLbl);
+            this.PersonalGroup.Controls.Add(this.DOBlbl);
+            this.PersonalGroup.Controls.Add(this.PhoneTxt);
+            this.PersonalGroup.Controls.Add(this.PHlbl);
+            this.PersonalGroup.Controls.Add(this.GenderLbl);
+            this.PersonalGroup.Controls.Add(this.LastNameTxt);
+            this.PersonalGroup.Location = new System.Drawing.Point(29, 81);
+            this.PersonalGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PersonalGroup.Name = "PersonalGroup";
+            this.PersonalGroup.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.PersonalGroup.Size = new System.Drawing.Size(306, 236);
+            this.PersonalGroup.TabIndex = 21;
+            this.PersonalGroup.TabStop = false;
+            this.PersonalGroup.Text = "Personal details";
+            // 
+            // EmailTxt
+            // 
+            this.EmailTxt.Location = new System.Drawing.Point(117, 197);
+            this.EmailTxt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.EmailTxt.Name = "EmailTxt";
+            this.EmailTxt.Size = new System.Drawing.Size(162, 20);
+            this.EmailTxt.TabIndex = 17;
+            // 
+            // Emaillbl
+            // 
+            this.Emaillbl.AutoSize = true;
+            this.Emaillbl.Location = new System.Drawing.Point(64, 200);
+            this.Emaillbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Emaillbl.Name = "Emaillbl";
+            this.Emaillbl.Size = new System.Drawing.Size(32, 13);
+            this.Emaillbl.TabIndex = 16;
+            this.Emaillbl.Text = "Email";
+            // 
+            // DOB_Dtp
+            // 
+            this.DOB_Dtp.Location = new System.Drawing.Point(117, 98);
+            this.DOB_Dtp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DOB_Dtp.Name = "DOB_Dtp";
+            this.DOB_Dtp.Size = new System.Drawing.Size(162, 20);
+            this.DOB_Dtp.TabIndex = 15;
+            // 
+            // genderComboBox
+            // 
+            this.genderComboBox.FormattingEnabled = true;
+            this.genderComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Gender Diverse"});
+            this.genderComboBox.Location = new System.Drawing.Point(117, 132);
+            this.genderComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.genderComboBox.Name = "genderComboBox";
+            this.genderComboBox.Size = new System.Drawing.Size(162, 21);
+            this.genderComboBox.TabIndex = 14;
+            // 
+            // GenderLbl
+            // 
+            this.GenderLbl.AutoSize = true;
+            this.GenderLbl.Location = new System.Drawing.Point(56, 135);
+            this.GenderLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GenderLbl.Name = "GenderLbl";
+            this.GenderLbl.Size = new System.Drawing.Size(42, 13);
+            this.GenderLbl.TabIndex = 11;
+            this.GenderLbl.Text = "Gender";
+            // 
+            // CourseGroup
+            // 
+            this.CourseGroup.Controls.Add(this.courseComboBox);
+            this.CourseGroup.Location = new System.Drawing.Point(29, 344);
+            this.CourseGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CourseGroup.Name = "CourseGroup";
+            this.CourseGroup.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CourseGroup.Size = new System.Drawing.Size(306, 70);
+            this.CourseGroup.TabIndex = 22;
+            this.CourseGroup.TabStop = false;
+            this.CourseGroup.Text = "Chosen Course";
+            // 
+            // courseComboBox
+            // 
+            this.courseComboBox.FormattingEnabled = true;
+            this.courseComboBox.Items.AddRange(new object[] {
+            "L5 ITTS (Information Technology Technical Support)",
+            "L5 IS (Information Systems)",
+            "L6 SD (Software Development)",
+            "L6 SA (System Administration)",
+            "L7 CM (Cloud Management)"});
+            this.courseComboBox.Location = new System.Drawing.Point(26, 31);
+            this.courseComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.courseComboBox.Name = "courseComboBox";
+            this.courseComboBox.Size = new System.Drawing.Size(254, 21);
+            this.courseComboBox.TabIndex = 0;
+            // 
+            // ListedInfoGroup
+            // 
+            this.ListedInfoGroup.Controls.Add(this.FinalOutputListBox);
+            this.ListedInfoGroup.Location = new System.Drawing.Point(355, 81);
+            this.ListedInfoGroup.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ListedInfoGroup.Name = "ListedInfoGroup";
+            this.ListedInfoGroup.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ListedInfoGroup.Size = new System.Drawing.Size(284, 384);
+            this.ListedInfoGroup.TabIndex = 23;
+            this.ListedInfoGroup.TabStop = false;
+            this.ListedInfoGroup.Text = "Your Course Enrollment Information";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 485);
+            this.Controls.Add(this.CourseGroup);
+            this.Controls.Add(this.PersonalGroup);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ListedInfoGroup);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Home";
+            this.Text = "Course Enrollment";
+            this.PersonalGroup.ResumeLayout(false);
+            this.PersonalGroup.PerformLayout();
+            this.CourseGroup.ResumeLayout(false);
+            this.ListedInfoGroup.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNameTxt;
+        private System.Windows.Forms.Label FNLbl;
+        private System.Windows.Forms.Label DOBlbl;
+        private System.Windows.Forms.Label PHlbl;
+        private System.Windows.Forms.TextBox PhoneTxt;
+        private System.Windows.Forms.Label LNlbl;
+        private System.Windows.Forms.TextBox LastNameTxt;
+        private System.Windows.Forms.ListBox FinalOutputListBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.GroupBox PersonalGroup;
+        private System.Windows.Forms.Label GenderLbl;
+        private System.Windows.Forms.GroupBox CourseGroup;
+        private System.Windows.Forms.ComboBox genderComboBox;
+        private System.Windows.Forms.DateTimePicker DOB_Dtp;
+        private System.Windows.Forms.TextBox EmailTxt;
+        private System.Windows.Forms.Label Emaillbl;
+        private System.Windows.Forms.ComboBox courseComboBox;
+        private System.Windows.Forms.GroupBox ListedInfoGroup;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+Scenario - Course Enrolment App:
+Tech-Geeks, a software development company in Auckland, specializes in creating software applications. Tech-Geeks have been approached by Hamilton school to develop a desktop application for student course enrolment. 
+They are asking for a desktop application where a student can enrol into a course by filling all his personal details along with their expression of interest in a course. The enrolment form should offer them with current available course like IS, SD, ITTS, ITSA, CM. 
+Client Requirements
+The following are the client requirements for the project “Course-Enrolment-App”.
+1.	Enrolment Form: The application should include a form where students can enter their details, such as:
+•	First Name
+•	Last Name
+•	Date of Birth
+•	Email Address
+•	Gender
+•	Phone Number
+2.	Course Selection: Students should be able to select and enrol in courses available at the school. They can enrol in multiple courses, but only one course at a time.
+3.	Display the selected courses on the screen: The application should display a list of courses the student has expressed interest in on the screen.
+4.	Form Reset After Enrolment: After an enrolment request is submitted, the form should reset to empty, allowing the student to enrol in a new course.
+5.	Delete Option for Courses: The application should include a delete button, allowing students to remove a specific course from their list of enrolled courses if they wish to drop out of course.
+These features will help streamline the enrolment process for students at Hamilton school, providing a user-friendly and efficient solution for course registration.
+
+As a member of the Tech-Geeks software development team you involve in various stages of the software development life cycle and responsible for producing desired outcomes as stated in the solution requirements in tasks 2,3 and 4 given below.    
+
+
+
